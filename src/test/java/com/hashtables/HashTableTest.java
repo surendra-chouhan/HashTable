0940c1cd209973d44ef62a233f11c89ddf8576a1
surendra-chouhan <chouhansurendra028@gmail.com>
2021-03-16 11:40:43 +0530

    Added a TestCase to count the occurence of words in a Paragraph

--- src/test/java/com/hashtables/HashTableTest.java
+++ src/test/java/com/hashtables/HashTableTest.java
@@ -1,16 +1,20 @@
 package com.hashtables;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Locale;
-
 public class HashTableTest {
+    HashTable<String,Integer> hashTable;
+
+    @Before
+    public void set() {
+        hashTable = new HashTable<>();
+    }
 
     @Test
-    public void find_frequency_of_given_words() {
+    public void find_frequency_of_given_words_in_a_sentence() {
         String sentence = "To be or not to be";
-        HashTable<String,Integer> hashTable = new HashTable<>();
 
         String[] words = sentence.toLowerCase().split(" ");
 
@@ -25,6 +29,30 @@ public class HashTableTest {
             hashTable.add(word,value);
         }
         int frequency = hashTable.get("be");
+        System.out.println(hashTable);
+        Assert.assertEquals(2, frequency);
+    }
+
+    @Test
+    public void find_frequency_of_given_words_in_a_paragraph() {
+        String paragraph = "Paranoids are not"+
+                "paranoid because they are paranoid but"+
+                "because they keep putting themselves"+
+                "deliberately into paranoid avoidable"+
+                "situations";
+
+        String[] words = paragraph.toLowerCase().split(" ");
+
+        for (String word : words) {
+            Integer value = hashTable.get(word);
+            if(value == null)
+                value = 1;
+            else
+                value = value + 1;
+
+            hashTable.add(word,value);
+        }
+        int frequency = hashTable.get("are");
         System.out.println(hashTable);
         Assert.assertEquals(2, frequency);
     }
